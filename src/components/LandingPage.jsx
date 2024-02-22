fe92b2b4d99bdc02e63511d7f27c5fa58a922601
Uday Tondwal <udaytondwal1@gmail.com>
2024-02-22 23:40:14 +0530

    img change

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
                   >
                     <img
                       className="bg-cover object-fill origin-left"
-                      src="https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_1280.jpg"
+                      src="https://cdn.pixabay.com/photo/2017/02/20/15/15/check-2082845_1280.jpg"
                       alt="image"
                     ></img>
                   </motion.div>
